e80f7928aee4c44adce8df55fb568a9afc836073
ruslanuskembaev <yuvelir06@gmail.com>
2023-02-27 04:33:41 +0700

    deljs

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 function func() {
   let a = document.querySelector("#input");
-  // console.log(a.value);
-
-  const chars = a.split("");
-  console.log(chars);
-
   document.querySelector("#output").textContent = a.value;
 }
