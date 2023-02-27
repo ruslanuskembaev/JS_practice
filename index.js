01cc959f3d7b84252124b4082de7405557ae0a16
ruslanuskembaev <yuvelir06@gmail.com>
2023-02-27 21:56:45 +0700

    layout

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function func() {
-  let a = document.querySelector("#input");
-  document.querySelector("#output").textContent = a.value;
+  let a1 = document.querySelector("#input1");
+  let a2 = document.querySelector("#input2");
+  let a3 = document.querySelector("#input3");
+  let a4 = document.querySelector("#input4");
+  let a5 = document.querySelector("#input5");
+
+  document.querySelector("#output1").textContent = a1.value;
+  document.querySelector("#output2").textContent = a2.value;
+  document.querySelector("#output3").textContent = a3.value;
+  document.querySelector("#output4").textContent = a4.value;
+  document.querySelector("#output5").textContent = a5.value;
 }
